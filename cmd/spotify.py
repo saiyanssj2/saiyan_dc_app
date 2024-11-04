3a9e1d52a3a82e30c421c13c2b46486081d458d3
saiyan_asus <hohuuminh98@gmail.com>
2024-11-04 19:16:41 +0700

    test spo

---
+++ cmd/spotify.py
@@ -0,0 +1,52 @@
+import os
+import discord
+import asyncio
+from dotenv import load_dotenv
+from discord import app_commands
+from concurrent.futures import ThreadPoolExecutor
+from spotdl import Spotdl
+from lib import dc_nextsong, dc_queue
+
+music_queues = {}
+executor = ThreadPoolExecutor()
+# Load biến môi trường từ file .env
+load_dotenv()
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+spotdl = Spotdl(CLIENT_ID,CLIENT_SECRET)
+
+def setup(bot):
+    @bot.tree.command(name="s", description="Phát nhạc từ Spotify")
+    @app_commands.describe(key="URL hoặc từ khóa Spotify")
+    async def spotify(interaction: discord.Interaction, key: str):
+        await interaction.response.defer()
+
+        user = interaction.user
+        if user.voice:
+            channel = user.voice.channel
+            if channel.id not in music_queues:
+                music_queues[channel.id] = dc_queue.MusicQueue()
+
+            music_queue = music_queues[channel.id]
+            loop = asyncio.get_event_loop()
+
+            # Sử dụng spotdl để tìm URL phát nhạc từ Spotify
+            async def fetch_spotify_data(key):
+                return await loop.run_in_executor(executor, lambda: spotdl.search(key))
+
+            spotify_url = await fetch_spotify_data(key)
+            if spotify_url:
+                song = {"title": spotify_url['name'], "url": spotify_url['url']}
+                music_queue.add((song['title'], song['url']))
+                
+                # Kết nối voice client và phát nhạc nếu chưa có
+                voice_client = discord.utils.get(bot.voice_clients, guild=interaction.guild)
+                if not voice_client:
+                    voice_client = await channel.connect()
+                    await dc_nextsong.play_next(voice_client, music_queue, interaction)
+                else:
+                    await interaction.followup.send(f"Queued!")
+            else:
+                await interaction.followup.send("Không tìm thấy bài hát trên Spotify.")
+        else:
+            await interaction.followup.send("Bạn cần tham gia kênh voice để phát nhạc.")
--- cmd/test.py
+++ cmd/test.py
@@ -2,8 +2,8 @@ import discord
 import asyncio
 from discord import app_commands
 from yt_dlp import YoutubeDL
-from collections import deque
 from concurrent.futures import ThreadPoolExecutor
+from lib import dc_nextsong, dc_queue
 
 # Cấu hình YoutubeDL
 ydl_opts = {
@@ -15,29 +15,6 @@ ydl_opts = {
     "default_search": "ytsearch",
     "extract_flat": "in_playlist"
 }
-
-ffmpeg_options = {
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-    'options': '-vn -filter:a "volume=0.25"'
-}
-
-# Khởi tạo hàng đợi cho âm thanh
-class MusicQueue:
-    def __init__(self):
-        self.queue = deque()
-        self.title = ""
-        self.url = ""
-        self.is_playing = False
-
-    def add(self, item):
-        self.queue.append(item)
-
-    def get_next(self):
-        if self.queue:
-            next_song = self.queue.popleft()
-            self.title, self.url = next_song
-            return next_song
-        return None
 
 music_queues = {}  # Tạo một dictionary để lưu hàng đợi cho mỗi kênh
 executor = ThreadPoolExecutor()
@@ -54,7 +31,7 @@ def setup(bot):
 
             # Tạo hàng đợi nếu chưa có
             if channel.id not in music_queues:
-                music_queues[channel.id] = MusicQueue()
+                music_queues[channel.id] = dc_queue.MusicQueue()
 
             music_queue = music_queues[channel.id]
             loop = asyncio.get_event_loop()
@@ -79,35 +56,10 @@ def setup(bot):
             voice_client = discord.utils.get(bot.voice_clients, guild=interaction.guild)
             if not voice_client:
                 voice_client = await channel.connect()
-                await play_next(voice_client, music_queue)
-                await interaction.followup.send(f"Now playing: {music_queue.title}")
+                await dc_nextsong.play_next(voice_client, music_queue, interaction)
             else:
-                await interaction.followup.send(f"Queued:")
+                await interaction.followup.send(f"Queued!")
         else:
             await interaction.followup.send("Bạn cần tham gia kênh voice để phát nhạc.")
 
-    async def play_next(voice_client, music_queue):
-        # Kiểm tra xem có bài nào trong hàng đợi không
-        next_song = music_queue.get_next()
-        if next_song:
-            title, url = next_song
-            music_queue.is_playing = True
-            print(f'Now playing: {title}')
-            
-            # Phát nhạc và xử lý callback khi nhạc kết thúc
-            def on_song_end(error):
-                # Nếu có lỗi, in ra lỗi
-                if error:
-                    print(f'Error: {error}')
-                # Sử dụng asyncio để lên lịch cho bài hát tiếp theo sau khi kết thúc
-                future = asyncio.run_coroutine_threadsafe(play_next(voice_client, music_queue), bot.loop)
-                try:
-                    future.result()
-                except Exception as e:
-                    print(f"Error scheduling next song: {e}")
-            audio_source = discord.FFmpegPCMAudio(url, **ffmpeg_options)
-            voice_client.play(audio_source, after=on_song_end)
-        else:
-            music_queue.is_playing = False
-            # Ngắt kết nối nếu không còn bài hát
-            await voice_client.disconnect()
+    
